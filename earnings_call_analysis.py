3444915fa0c8c3f62d82b89c48165c0079e046c3
nishanthrs <nishanth.salinamakki@gmail.com>
2019-03-18 22:37:40 -0700

    Added some additional cleaning; implemented part of NLP cleaning pipeline

--- earnings_call_analysis.py
+++ earnings_call_analysis.py
@@ -1,13 +1,32 @@
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+import nltk
+import spacy
+
+# nltk.download('stopwords')
+# stopwords = spacy.lang.en.stop_words.STOP_WORDS
+nlp = spacy.load('en')
+punct_marks = ['.', ',', '?', '!', '*', '-', '\n', '\n\n']
+
+
+def add_punct_stopwords(punct_marks):
+    for punct in punct_marks:
+        nlp.vocab[punct].is_stop = True
 
 
 # Method that implements NLP pipeline of cleaning text
 def clean_earnings_call_transcript(local_file):
     transcript_read = open(local_file, 'r')
-    transcript_text = transcript_read.read()
 
+    # 1. Tokenize
+    transcript_text = nlp(transcript_read.read())
+    # 2. Remove stopwords
+    add_punct_stopwords(punct_marks)
+    transcript_text = [
+        token.text for token in transcript_text if not token.is_stop
+    ]
     print(transcript_text)
+    # 3. Lemmatization
 
 
-clean_earnings_call_transcript('crm_earnings_transcript_clean.txt')
+clean_earnings_call_transcript('crm_earnings_transcript_clean_2.txt')
--- earnings_call_scraper.py
+++ earnings_call_scraper.py
@@ -1,6 +1,7 @@
 import requests
 import html2text
 import random
+import re
 '''
 # References: 
 #   1. Scraping Seeking Alpha pages: https://stackoverflow.com/questions/48756326/web-scraping-results-in-403-forbidden-error
@@ -23,6 +24,7 @@ def get_earnings_call_transcript(transcript_uri):
             rand_proxy = get_rand_ip_address()
             print("Proxy used: ", rand_proxy)
             r = requests.get(transcript_uri, proxies={'http': rand_proxy}).text
+            # TODO: add mechanism here to keep successful_req as False if robot page appears
             successful_req = True
         except requests.ConnectionError as e:
             print("OOPS!! Connection Error. Technical Details given below.\n")
@@ -37,20 +39,23 @@ def get_earnings_call_transcript(transcript_uri):
             print("Someone closed the program")
             break
 
-    crm = open('crm_earnings_transcript.txt', 'w+')
+    crm = open('crm_earnings_transcript_2.txt', 'w+')
     crm.write(r)
     crm.close()
 
 
 def clean_earnings_call_transcript_html(local_file):
     crm_read = open(local_file, 'r')
-    # print(crm_read.read())
-
     cleaned_earnings_transcript = html2text.html2text(crm_read.read())
     crm_read.close()
-    # print(cleaned_earnings_transcript)
-    crm_clean = open('crm_earnings_transcript_clean.txt', 'w+')
-    crm_clean.write(cleaned_earnings_transcript)
+    crm_clean = open('crm_earnings_transcript_clean_2.txt', 'w+')
+
+    a = re.search(r'\b(Company Participants)\b',
+                  cleaned_earnings_transcript).start()
+    b = re.search(r'\b(Follow SA Transcripts and get email alerts)\b',
+                  cleaned_earnings_transcript).start()
+
+    crm_clean.write(cleaned_earnings_transcript[(a - 2):b])
     crm_clean.close()
 
 
@@ -58,4 +63,4 @@ get_earnings_call_transcript(
     'https://seekingalpha.com/article/4246320-salesforce-com-inc-crm-ceo-marc-benioff-q4-2019-results-earnings-call-transcript'
 )
 
-clean_earnings_call_transcript_html('crm_earnings_transcript.txt')
+clean_earnings_call_transcript_html('crm_earnings_transcript_2.txt')
